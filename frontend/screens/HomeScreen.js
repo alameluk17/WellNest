ba43bcababac416f0dc61ada758a98c1dbc0cab7
Jahnavi Murali <109456718+jahnavimurali@users.noreply.github.com>
2024-05-04 20:13:59 +0530

    upload files to firestore

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,10 +1,51 @@
-import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+import { View, Text, Pressable, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from 'react';
+import * as DocumentPicker from 'expo-document-picker';
+import {app} from '../database/firebaseconfig'
+import { getStorage, ref, uploadBytes } from 'firebase/storage'; 
 
 export default function HomeScreen({navigation}){
-    return(
-        <View>
-            <Text>Home Screen</Text>
-            <Text>Whaaaaaattt</Text>
-        </View>
-    )
-}
+    const storage = getStorage(app);
+    const handlePickDocument = async () => {
+        try {
+            console.log('Hello World')
+          const result = await DocumentPicker.getDocumentAsync({
+            type: '*/*', 
+          });
+    
+          if (result && !result.cancelled) {
+            const assets = result["assets"][0]
+            const uri = assets["uri"]
+            const name = assets["name"]
+            const storageRef = ref(storage);
+            const fileRef = ref(storageRef, name); 
+            const response = await fetch(uri);
+            const blob = await response.blob();
+            await uploadBytes(fileRef, blob);
+    
+            console.log('File uploaded successfully!');
+          }
+        } catch (error) {
+        //   console.error('Error picking document:', error);
+        }
+      };
+    
+      return (
+        <TouchableOpacity style={styles.buttonContainer} onPress={handlePickDocument}>
+          <Text style={styles.buttonText}>Upload File</Text>
+        </TouchableOpacity>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      buttonContainer: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+        margin: 70,
+        borderRadius: 5,
+      },
+      buttonText: {
+        fontSize: 16,
+      },
+    });
